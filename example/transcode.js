b00ff07825677f588e64286eb1e015815a6e21ab
Andrew Kelley <superjoe30@gmail.com>
2013-11-18 11:21:26 -0500

    add missing uv_mutex_init and uv_cond_init calls to encoder

---
+++ example/transcode.js
@@ -0,0 +1,53 @@
+/* transcode a file into ogg vorbis */
+
+var groove = require('../');
+var assert = require('assert');
+var fs = require('fs');
+
+if (process.argv.length < 4) usage();
+
+groove.setLogging(groove.LOG_INFO);
+
+var playlist = groove.createPlaylist();
+var encoder = groove.createEncoder();
+encoder.formatShortName = "ogg";
+encoder.codecShortName = "vorbis";
+
+var outStream = fs.createWriteStream(process.argv[3]);
+
+encoder.on('buffer', function() {
+  var buffer;
+  while (buffer = encoder.getBuffer()) {
+    if (buffer.buffer) {
+      outStream.write(buffer.buffer);
+    } else {
+      cleanup();
+      return;
+    }
+  }
+});
+
+encoder.attach(playlist, function(err) {
+  assert.ifError(err);
+
+  groove.open(process.argv[2], function(err, file) {
+    assert.ifError(err);
+    playlist.insert(file, null);
+  });
+});
+
+function cleanup() {
+  var file = playlist.items()[0].file;
+  playlist.clear();
+  file.close(function(err) {
+    assert.ifError(err);
+    encoder.detach(function(err) {
+      assert.ifError(err);
+    });
+  });
+}
+
+function usage() {
+  console.error("Usage: node transcode.js inputfile outputfile");
+  process.exit(1);
+}
--- src/gn_encoder.cc
+++ src/gn_encoder.cc
@@ -123,6 +123,8 @@ static void AttachAsync(uv_work_t *req) {
     }
 
     GNEncoder::EventContext *context = r->event_context;
+    uv_cond_init(&context->cond);
+    uv_mutex_init(&context->mutex);
 
     uv_async_init(uv_default_loop(), &context->event_async, EventAsyncCb);
     context->event_async.data = context;
