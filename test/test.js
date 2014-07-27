dbf66d3543d6acfc3b8cca7d76d4e1311a8ed862
Andrew Kelley <superjoe30@gmail.com>
2014-07-26 17:22:56 -0700

    switch to mocha for testing

--- test/test.js
+++ test/test.js
@@ -3,144 +3,142 @@ var assert = require('assert');
 var path = require('path');
 var fs = require('fs');
 var ncp = require('ncp').ncp;
-var test = require('tap').test;
 var testOgg = path.join(__dirname, "danse.ogg");
 var bogusFile = __filename;
 var rwTestOgg = path.join(__dirname, "danse-rw.ogg");
+var it = global.it;
 
-test("version", function(t) {
+it("version", function() {
   var ver = groove.getVersion();
-  t.strictEqual(typeof ver.major, 'number');
-  t.strictEqual(typeof ver.minor, 'number');
-  t.strictEqual(typeof ver.patch, 'number');
-  t.end();
+  assert.strictEqual(typeof ver.major, 'number');
+  assert.strictEqual(typeof ver.minor, 'number');
+  assert.strictEqual(typeof ver.patch, 'number');
 });
 
-test("logging", function(t) {
-    t.strictEqual(groove.LOG_ERROR, 16);
+it("logging", function() {
+    assert.strictEqual(groove.LOG_ERROR, 16);
     groove.setLogging(groove.LOG_INFO);
-    t.end();
 });
 
-test("open fails for bogus file", function(t) {
-    t.plan(1);
+it("open fails for bogus file", function(done) {
     groove.open(bogusFile, function(err, file) {
-        t.equal(err.message, "open file failed");
+        assert.strictEqual(err.message, "open file failed");
+        done();
     });
 });
 
-test("open file and read metadata", function(t) {
-    t.plan(10);
+it("open file and read metadata", function(done) {
     groove.open(testOgg, function(err, file) {
-        t.ok(!err);
-        t.ok(file.id);
-        t.equal(file.filename, testOgg);
-        t.equal(file.dirty, false);
-        t.equal(file.metadata().TITLE, 'Danse Macabre');
-        t.equal(file.metadata().ARTIST, 'Kevin MacLeod');
-        t.equal(file.shortNames(), 'ogg');
-        t.equal(file.getMetadata('initial key'), 'C');
-        t.equal(file.getMetadata('bogus nonexisting tag'), null);
+        assert.ok(!err);
+        assert.ok(file.id);
+        assert.strictEqual(file.filename, testOgg);
+        assert.strictEqual(file.dirty, false);
+        assert.strictEqual(file.metadata().TITLE, 'Danse Macabre');
+        assert.strictEqual(file.metadata().ARTIST, 'Kevin MacLeod');
+        assert.strictEqual(file.shortNames(), 'ogg');
+        assert.strictEqual(file.getMetadata('initial key'), 'C');
+        assert.equal(file.getMetadata('bogus nonexisting tag'), null);
         file.close(function(err) {
-            t.ok(!err);
+            assert.ok(!err);
+            done();
         });
     });
 });
 
-test("update metadata", function(t) {
-    t.plan(7);
+it("update metadata", function(done) {
     ncp(testOgg, rwTestOgg, function(err) {
-        t.ok(!err);
+        assert.ok(!err);
         groove.open(rwTestOgg, doUpdate);
     });
     function doUpdate(err, file) {
-        t.ok(!err);
+        assert.ok(!err);
         file.setMetadata('foo new key', "libgroove rules!");
-        t.equal(file.getMetadata('foo new key'), 'libgroove rules!');
+        assert.strictEqual(file.getMetadata('foo new key'), 'libgroove rules!');
         file.save(function(err) {
-            t.ok(!err);
+            assert.ok(!err);
             file.close(checkUpdate);
         });
     }
     function checkUpdate(err) {
-        t.ok(!err);
+        assert.ok(!err);
         groove.open(rwTestOgg, function(err, file) {
-            t.ok(!err);
-            t.equal(file.getMetadata('foo new key'), 'libgroove rules!', "update worked");
+            assert.ok(!err);
+            assert.strictEqual(file.getMetadata('foo new key'), 'libgroove rules!');
             fs.unlinkSync(rwTestOgg);
+            done();
         });
     }
 });
 
-test("create empty playlist", function (t) {
-    t.plan(2);
+it("create empty playlist", function (done) {
     var playlist = groove.createPlaylist();
-    t.ok(playlist.id);
-    t.equivalent(playlist.items(), [], "empty playlist");
+    assert.ok(playlist.id);
+    assert.deepEqual(playlist.items(), []);
+    done();
 });
 
-test("create empty player", function (t) {
-    t.plan(2);
+it("create empty player", function (done) {
     var player = groove.createPlayer();
-    t.ok(player.id);
-    t.equal(player.targetAudioFormat.sampleRate, 44100);
+    assert.ok(player.id);
+    assert.strictEqual(player.targetAudioFormat.sampleRate, 44100);
+    done();
 });
 
-test("playlist item ids", function(t) {
-    t.plan(8);
+it("playlist item ids", function(done) {
     var playlist = groove.createPlaylist();
-    t.ok(playlist);
+    assert.ok(playlist);
     playlist.pause();
-    t.equal(playlist.playing(), false);
+    assert.equal(playlist.playing(), false);
     groove.open(testOgg, function(err, file) {
-        t.ok(!err, "opening file");
-        t.ok(playlist.position);
-        t.equal(playlist.gain, 1.0);
+        assert.ok(!err, "opening file");
+        assert.ok(playlist.position);
+        assert.strictEqual(playlist.gain, 1.0);
         playlist.setGain(1.0);
         var returned1 = playlist.insert(file, null);
         var returned2 = playlist.insert(file, null);
         var items1 = playlist.items();
         var items2 = playlist.items();
-        t.equal(items1[0].id, items2[0].id);
-        t.equal(items1[0].id, returned1.id);
-        t.equal(items2[1].id, returned2.id);
+        assert.strictEqual(items1[0].id, items2[0].id);
+        assert.strictEqual(items1[0].id, returned1.id);
+        assert.strictEqual(items2[1].id, returned2.id);
+        done();
     });
 });
 
-test("create, attach, detach player", function(t) {
-    t.plan(2);
+it("create, attach, detach player", function(done) {
     var playlist = groove.createPlaylist();
     var player = groove.createPlayer();
     player.attach(playlist, function(err) {
-        t.ok(!err, "attach");
+        assert.ok(!err);
         player.detach(function(err) {
-            t.ok(!err, "detach");
+            assert.ok(!err);
+            done();
         });
     });
 });
 
-test("create, attach, detach loudness detector", function(t) {
-  t.plan(2);
+it("create, attach, detach loudness detector", function(done) {
   var playlist = groove.createPlaylist();
   var detector = groove.createLoudnessDetector();
   detector.attach(playlist, function(err) {
-    t.ok(!err, "attach");
+    assert.ok(!err);
     detector.detach(function(err) {
-      t.ok(!err, "detach");
+      assert.ok(!err);
+      done();
     });
   });
 });
 
-test("create, attach, detach encoder", function(t) {
-    t.plan(2);
+it("create, attach, detach encoder", function(done) {
     var playlist = groove.createPlaylist();
     var encoder = groove.createEncoder();
     encoder.formatShortName = "ogg";
     encoder.codecShortName = "vorbis";
     encoder.attach(playlist, function(err) {
-        t.ok(!err, "attach");
+        assert.ok(!err);
         encoder.detach(function(err) {
-            t.ok(!err, "detach");
+            assert.ok(!err);
+            done();
         });
     });
 });
